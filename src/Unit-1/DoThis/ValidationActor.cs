47793d36ecb6bb0e9113fe69e7472fdf1fa9e274
Jun Nakajima <ikuta1919@gmail.com>
2020-11-04 12:59:40 +0900

    ValidationActorを作成し、Programの中を綺麗にした

---
+++ src/Unit-1/DoThis/ValidationActor.cs
@@ -0,0 +1,50 @@
+using Akka.Actor;
+
+namespace WinTail
+{
+    public class ValidationActor : UntypedActor
+    {
+        private readonly IActorRef _consoleWriterActor;
+
+        public ValidationActor(IActorRef consoleWriterActor)
+        {
+            _consoleWriterActor = consoleWriterActor;
+        }
+
+        protected override void OnReceive(object message)
+        {
+            var msg = message as string;
+            if (string.IsNullOrEmpty(msg))
+            {
+                _consoleWriterActor.Tell(new Messages.NullInputError("No input received."));
+            }
+            else
+            {
+                var valid = IsValid(msg);
+                if (valid)
+                {
+                    _consoleWriterActor.Tell(new Messages.InputSuccess("Thank you! Message was valid."));
+
+                    // continue reading messages from console
+                    Self.Tell(new Messages.ContinueProcessing());
+                }
+                else
+                {
+                    Self.Tell(new Messages.ValidationError("Invalid: input had odd number of characters."));
+                }
+            }
+        }
+        /// <summary>
+        /// Validates <see cref="message"/>.
+        /// Determines if the message received is valid.
+        /// Checks if number of chars in message received is even.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        private static bool IsValid(string message)
+        {
+            var valid = message.Length % 2 == 0;
+            return valid;
+        }
+    }
+}
